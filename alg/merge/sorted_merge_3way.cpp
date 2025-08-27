393f98ccd889bc08c67423d651d2c65e1876fdf9
vboxuser <vboxuser@Alg.local>
2025-08-27 23:41:14 +0000

    sorted_merged done

--- alg/merge/sorted_merge_3way.cpp
+++ alg/merge/sorted_merge_3way.cpp
@@ -9,13 +9,47 @@ sorted_merge_3way(
     const int *list_c, int nc,
     int *list_abc)
 {
+  int i = 0, j = 0, k = 0, m = 0;
 
-  /* 
+  for(int count = 0; count < na-1;count++)
+  {
+    if(list_a[count]> list_a[count+1])
+    {
+      return false;
+    }
+  }   
+  for(int count = 0; count < nb-1;count++)
+  {
+    if(list_b[count]> list_b[count+1])
+    {
+      return false;
+    }
+  } 
+  for(int count = 0; count < nc-1;count++)
+  {
+    if(list_c[count]> list_c[count+1])
+    {
+      return false;
+    }
+  }
 
-   Entrar coom aqui 
-   <++>
-   
-  */
+  while (i < na || j < nb || k < nc) {
+    int val_a = (i < na) ? list_a[i] : __INT_MAX__;
+    int val_b = (j < nb) ? list_b[j] : __INT_MAX__;
+    int val_c = (k < nc) ? list_c[k] : __INT_MAX__;
+    
 
-  return false; // stub
+    if (val_a <= val_b && val_a <= val_c) {
+      list_abc[m++] = val_a;
+      i++;
+     }else if (val_b <= val_a && val_b <= val_c) {
+        list_abc[m++] = val_b;
+        j++;
+     }else {
+        list_abc[m++] = val_c;
+        k++;
+     }
+    }
+
+  return true;
 }
