59f6b845a943de95ea4985eb9373ea7ffa76a24c
vboxuser <vboxuser@Alg.local>
2025-08-28 21:07:57 +0000

    sorted_merged a little more optmized

--- alg/merge/sorted_merge_3way.cpp
+++ alg/merge/sorted_merge_3way.cpp
@@ -11,34 +11,12 @@ sorted_merge_3way(
 {
   int i = 0, j = 0, k = 0, m = 0;
 
-  for(int count = 0; count < na-1;count++)
-  {
-    if(list_a[count]> list_a[count+1])
-    {
-      return false;
-    }
-  }   
-  for(int count = 0; count < nb-1;count++)
-  {
-    if(list_b[count]> list_b[count+1])
-    {
-      return false;
-    }
-  } 
-  for(int count = 0; count < nc-1;count++)
-  {
-    if(list_c[count]> list_c[count+1])
-    {
-      return false;
-    }
-  }
 
   while (i < na || j < nb || k < nc) {
     int val_a = (i < na) ? list_a[i] : __INT_MAX__;
     int val_b = (j < nb) ? list_b[j] : __INT_MAX__;
     int val_c = (k < nc) ? list_c[k] : __INT_MAX__;
     
-
     if (val_a <= val_b && val_a <= val_c) {
       list_abc[m++] = val_a;
       i++;
@@ -49,6 +27,13 @@ sorted_merge_3way(
         list_abc[m++] = val_c;
         k++;
      }
+    if(m!=1)
+    {
+       if(list_abc[m-1]<list_abc[m-2])
+       {
+	 return false;
+       }
+    }
     }
 
   return true;
